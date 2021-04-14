6a95db3eb877766f8b8932f2577ed4e30a28099c
Andrew <awcrotwell@gmail.com>
2021-04-14 14:55:13 -0400

    Vector postgres config

---
+++ src/vector/configs/postgres.ts
@@ -0,0 +1,57 @@
+import globalState from '../../state/globalState'
+
+const accessLogs = () => {
+  return `
+\n################ Postgres Logs #############################
+
+[sources.nginx_access_logs]
+type = "file"
+include = [
+  ${globalState.Vector.Postgres?.accessLog ? '"/var/log/nginx/access_log.log",' : null}
+  ${globalState.Vector.Postgres?.errorLog ? '"/var/log/nginx/error_log.log"' : null}
+]
+read_from = "beginning"
+ignore_checkpoints = true
+
+[transforms.nginx_access_logs_add_transform]
+type = "remap"
+inputs = ["nginx_access_logs"]
+source = '''
+.type = "nginx-access"
+'''
+
+[sinks.nginx_access_logs_firehose_stream_sink]
+# General
+type = "aws_kinesis_firehose" # required
+inputs = ["nginx_access_logs_add_transform"] # required
+region = "us-east-2" # required, required when endpoint = null
+stream_name = "DendroTestStream" # required
+auth.access_key_id = "${globalState.AWS.accessKeyId}"
+auth.secret_access_key = "${globalState.AWS.secretAccessKey}"
+# Encoding
+encoding.codec = "json" # required
+# Healthcheck
+healthcheck.enabled = true # optional, default
+
+#############################################\n
+    `
+}
+
+const metrics = () => {}
+
+export const buildConfig = () => {
+  let config = ''
+
+  if (globalState.Vector.Postgres?.metrics) {
+    config += metrics()
+  }
+
+  if (
+    globalState.Vector.Postgres?.accessLog ||
+    globalState.Vector.Postgres?.errorLog
+  ) {
+    config += accessLogs
+  }
+
+  return config
+}
