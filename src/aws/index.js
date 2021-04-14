97fc500810026b110a2d6845cb1f04ab3b8a3fa3
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-04-13 21:43:01 -0400

    Create lambda role and attach AWSLambdaBasicExecutionPolicy

--- src/aws/index.js
+++ src/aws/index.js
@@ -1,8 +1,10 @@
 const createBucket = require('./createBucket')
 const deleteBucket = require('./deleteBucket')
 const createLambda = require('./createLambda')
+const createLambdaRole = require('./createLambdaRole')
 const listLambdas = require('./listLambdas')
 const setLambdaInvokePolicy = require('./setLambdaInvokePolicy')
+const attachLambdaBasicExecutionPolicy = require('./attachLambdaBasicExecutionPolicy')
 const createS3LambdaTrigger = require('./createS3LambdaTrigger')
 const uploadToBucket = require('./uploadToBucket')
 const getCredentials = require('./getCredentials')
@@ -13,8 +15,10 @@ module.exports = {
   createBucket,
   deleteBucket,
   createLambda,
+  createLambdaRole,
   listLambdas,
   setLambdaInvokePolicy,
+  attachLambdaBasicExecutionPolicy,
   createS3LambdaTrigger,
   uploadToBucket,
   getCredentials,
--- src/index.js
+++ src/index.js
@@ -4,22 +4,33 @@
 
 const AWSWrapper = require('./aws')
 
-const newBucketName = 'dendrodefaultbucket2'
+const newRoleName = 'dendro-lambda-role'
+const newBucketName = 'dendrodefaultbucket'
 const pathToLambdaFunc = './aws/_deployableLambdaFunction.js'
 const fileToUpload = './aws/uploadToBucket.js';
 
 (async () => {
+  console.log('Creating new lambda role...')
+  const [lambdaRoleErr, lambdaRole] = await AWSWrapper.createLambdaRole(newRoleName)
+  // console.log(lambdaRoleErr)
+  // console.log(lambdaRole)
+
+  console.log('Attaching AWSLambdaBasicExecutionRole...')
+  const [attachPolicyErr, attachPolicyData] = await AWSWrapper.attachLambdaBasicExecutionPolicy(newRoleName)
+  // console.log(attachPolicyErr)
+  // console.log(attachPolicyData)
+
   console.log('Creating new bucket...')
   const [bucketErr, bucketData] = await AWSWrapper.createBucket(newBucketName)
   // console.log(bucketErr)
   // console.log(bucketData)
 
   console.log('Creating new lambda...')
-  const [lambdaErr, lambdaData] = await AWSWrapper.createLambda(pathToLambdaFunc, 'arn:aws:iam::141351053848:role/lambda_role')
+  const [lambdaErr, lambdaData] = await AWSWrapper.createLambda(pathToLambdaFunc, lambdaRole.Role.Arn)
   // console.log(lambdaErr)
   // console.log(lambdaData)
 
-  console.log('Setting lambda policy...')
+  console.log('Setting lambda invoke policy...')
   const [policyErr, policyData] = await AWSWrapper.setLambdaInvokePolicy(lambdaData.FunctionArn)
   // console.log(policyErr)
   // console.log(policyData)
@@ -31,8 +42,8 @@ const fileToUpload = './aws/uploadToBucket.js';
 
   console.log('Uploading to S3...')
   const [uploadErr, uploadData] = await AWSWrapper.uploadToBucket(newBucketName, fileToUpload)
-  // console.log(uploadErr)
-  // console.log(uploadData)
+  console.log(uploadErr)
+  console.log(uploadData)
 
   // const [credentialsErr, credentials] = await AWSWrapper.getCredentials()
   // // console.log(credentialsErr)
