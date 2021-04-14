935e36779b2617bce2900a808fe7026db31f92f2
Andrew <awcrotwell@gmail.com>
2021-04-14 18:16:17 -0400

    Apache & customApplication vector boilerplate

--- src/globalState/vector/customApplication.ts
+++ src/globalState/vector/customApplication.ts
@@ -1,1 +1,17 @@
-// TODO
+export interface CustomApplicationData {
+  name: string;
+  filepath: string;
+}
+
+class CustomApplication {
+  name: string;
+
+  filepath: string;
+
+  constructor({ name, filepath }: CustomApplicationData) {
+    this.name = name
+    this.filepath = filepath
+  }
+}
+
+export default CustomApplication
--- src/globalState/vector/index.ts
+++ src/globalState/vector/index.ts
@@ -3,6 +3,7 @@ import Nginx, { NginxData } from './nginx'
 import Apache, { ApacheData } from './apache'
 import Host, { HostData } from './host'
 import Mongo, { MongoData } from './mongo'
+import CustomApplication, { CustomApplicationData } from './customApplication'
 
 export interface VectorData {
   postgres?: PostgresData;
@@ -23,6 +24,8 @@ class Vector {
 
   Mongo: Mongo;
 
+  CustomApplications: CustomApplication[];
+
   constructor({
     postgres = {},
     nginx = {},
@@ -35,6 +38,13 @@ class Vector {
     this.Apache = new Apache(apache)
     this.Host = new Host(host)
     this.Mongo = new Mongo(mongo)
+    this.CustomApplications = []
+  }
+
+  setCustomApp(data: CustomApplicationData) {
+    this.CustomApplications.push(
+      new CustomApplication(data),
+    )
   }
 }
 
