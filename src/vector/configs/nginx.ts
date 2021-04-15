3b3020ea4c54f8d4c15ec558fe944c7f4ecfac1c
Andrew <awcrotwell@gmail.com>
2021-04-15 12:00:30 -0400

    Formatting

--- src/vector/configs/nginx.ts
+++ src/vector/configs/nginx.ts
@@ -5,35 +5,35 @@ const logConfig = (): string => {
 ################ Nginx Logs #############################
 
 [sources.nginx_logs]
-type = "file"
-include = [
-  ${store.Vector.Nginx.monitorAccessLogs ? '"/var/log/nginx/access_log.log",' : null}
-  ${store.Vector.Nginx.monitorErrorLogs ? '"/var/log/nginx/error_log.log"' : null}
-]
-read_from = "beginning"
+  type = "file"
+  include = [
+    ${store.Vector.Nginx.monitorAccessLogs ? '"/var/log/nginx/access_log.log",' : null}
+    ${store.Vector.Nginx.monitorErrorLogs ? '"/var/log/nginx/error_log.log"' : null}
+  ]
+  read_from = "beginning"
 
 [transforms.nginx_logs_transform]
-type = "remap"
-inputs = ["nginx_logs"]
-source = '''
-.type = "nginx-logs"
-'''
+  type = "remap"
+  inputs = ["nginx_logs"]
+  source = '''
+  .type = "nginx-logs"
+  '''
 
 [sinks.nginx_logs_firehose_stream_sink]
-# General
-type = "aws_kinesis_firehose"
-inputs = ["nginx_logs_transform"]
+  # General
+  type = "aws_kinesis_firehose"
+  inputs = ["nginx_logs_transform"]
 
-# AWS
-region = "us-east-2", required when endpoint = null
-stream_name = "NginxLogsDendroStream"
+  # AWS
+  region = "us-east-2", required when endpoint = null
+  stream_name = "NginxLogsDendroStream"
 
-## Auth
-auth.access_key_id = "${store.AWS.Credentials.accessKeyId}"
-auth.secret_access_key = "${store.AWS.Credentials.secretAccessKey}"
+  ## Auth
+  auth.access_key_id = "${store.AWS.Credentials.accessKeyId}"
+  auth.secret_access_key = "${store.AWS.Credentials.secretAccessKey}"
 
-# Encoding
-encoding.codec = "json"
+  # Encoding
+  encoding.codec = "json"
 
 #############################################
 `;
