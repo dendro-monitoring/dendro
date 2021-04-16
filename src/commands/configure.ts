083ea2e4c3253037eaa1c2a06a182cf8a8d4f2f1
nlicalzi <nicholas.licalzi@gmail.com>
2021-04-16 11:41:43 -0400

    added proper typing for promptConfig calls

--- src/commands/configure.ts
+++ src/commands/configure.ts
@@ -17,66 +17,79 @@ export default class Configure extends Command {
   static examples = [];
   static flags = {};
 
-  nginxConfig: any = async () => {
+  async nginxConfig(): Promise<void> {
+    console.clear();
+
     const nginxServices: string[] = await nginxPrompt.run();
 
     if (nginxServices.includes('Access log')) { store.Vector.Nginx.monitorAccessLogs = true; }
     if (nginxServices.includes('Error log')) { store.Vector.Nginx.monitorErrorLogs = true; }
     if (nginxServices.includes('Health metrics')) {
+      console.clear();
       const nginxHealth: any = await nginxHealthPrompt.run();
 
       store.Vector.Nginx.monitorMetrics = true;
       Object.assign(store.Vector.Nginx, nginxHealth);
     }
-  };
+  }
 
-  apacheConfig: any = async () => {
+  async apacheConfig(): Promise<void> {
+    console.clear();
+
     const apacheServices: string[] = await apachePrompt.run();
 
     if (apacheServices.includes('Access log')) { store.Vector.Apache.monitorAccessLogs = true; }
     if (apacheServices.includes('Error log')) { store.Vector.Apache.monitorErrorLogs = true; }
     if (apacheServices.includes('Health metrics')) {
+      console.clear();
       const apacheHealth: any = await apacheHealthPrompt.run();
 
       store.Vector.Apache.monitorMetrics = true;
       Object.assign(store.Vector.Apache, apacheHealth);
     }
-  };
+  }
 
-  postgresConfig: any = async () => {
-    const postgresServices: any = await postgresPrompt.run();
+  async postgresConfig(): Promise<void> {
+    console.clear();
+
+    const postgresServices: string[] = await postgresPrompt.run();
 
     if (postgresServices.includes('Error log')) { store.Vector.Postgres.monitorErrorLogs = true; }
     if (postgresServices.includes('Health metrics')) {
+      console.clear();
       const pgCreds: any = await postgresCredentialsPrompt.run();
 
       store.Vector.Postgres.monitorMetrics = true;
       Object.assign(store.Vector.Postgres, pgCreds);
     }
-  };
+  }
 
-  mongoConfig: any = async () => {
-    const mongoServices: any = await mongoPrompt.run();
+  async mongoConfig(): Promise<void> {
+    console.clear();
+    
+    const mongoServices: string[] = await mongoPrompt.run();
 
     if (mongoServices.includes('Log')) { store.Vector.Mongo.monitorLogs = true; }
     if (mongoServices.includes('Health metrics')) { 
+      console.clear();
       const mongoCreds: any = await mongoCredentialsPrompt.run();
 
       store.Vector.Mongo.monitorMetrics = true;
       Object.assign(store.Vector.Mongo, mongoCreds);
     }
-  };
+  }
 
-  hostConfig: any = async () => {
-    const hostServices: any = await hostPrompt.run();
+  async hostConfig(): Promise<void> {
+    console.clear();
 
+    const hostServices: string[] = await hostPrompt.run();
     const hostSelections = hostServices.reduce((map: any, obj: string) => {
       map[obj.toLowerCase()] = true;
       return map;
     }, {});
 
     Object.assign(store.Vector.Host, hostSelections);
-  };
+  }
 
   async run() {
     const { args, flags } = this.parse(Configure);
