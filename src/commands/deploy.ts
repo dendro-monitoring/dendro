cb0b1d459f97f6b1b75ea2f7dcbc9d24669a93be
Andrew <awcrotwell@gmail.com>
2021-04-19 10:03:29 -0400

    Remove comments in deploy command

--- src/commands/deploy.ts
+++ src/commands/deploy.ts
@@ -6,16 +6,9 @@ import { Command, flags } from '@oclif/command';
 import log, { LevelNames } from "../utils/log";
 import orchestrator from '../aws/orchestrator';
 
-// TODO put & use constants in globalState. Right now, constants aren't being shared between services
-//  so the connections are broken
-
 export default class DeployCommand extends Command {
   static flags = {
     help: flags.help({ char: 'h' }),
-    // flag with a value (-n, --name=VALUE)
-    // name: flags.string({ char: 'n', description: 'name to print' }),
-    // flag with no value (-f, --force)
-    // force: flags.boolean({ char: 'f' }),
     level: flags.string({
       char: 'L',
       description: 'set the log level',
