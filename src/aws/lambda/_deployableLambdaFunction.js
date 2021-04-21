cd6c81142e41786ad8e1984325ae30f0546cd4c3
Andrew <awcrotwell@gmail.com>
2021-04-21 13:46:19 -0400

    Update lambda to handle record types

--- src/aws/lambda/_deployableLambdaFunction.js
+++ src/aws/lambda/_deployableLambdaFunction.js
@@ -1,14 +1,65 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 
-console.log('Loading function');
-
 const aws = require('aws-sdk');
-
-const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 const https = require('https');
 
+/**
+ * The regex used to match individual records.
+ * Records coming in have no `,` so we replace any
+ * `}{` with `},{` to make them json parsable
+ */
 const REGEX = /}{/gm;
+
+const DATABASE_NAME = "DendroTimestreamDB";
+
+/**
+ * All the different vector types & database names
+ */
+const VECTOR_APACHE_LOGS_TYPE = "apache-logs";
+const VECTOR_APACHE_METRICS_TYPE = "apache-metrics";
+const VECTOR_CUSTOM_APPLICATION_TYPE = "custom-application";
+const VECTOR_HOST_METRICS_TYPE = "host-metrics";
+const VECTOR_MONGO_LOGS_TYPE = "mongo-logs";
+const VECTOR_MONGO_METRICS_TYPE = "mongo-metrics";
+const VECTOR_NGINX_LOGS_TYPE = "nginx-logs";
+const VECTOR_NGINX_METRICS_TYPE = "nginx-metrics";
+const VECTOR_POSTGRES_LOGS_TYPE = "postgres-logs";
+const VECTOR_POSTGRES_METRICS_TYPE = "postgres-metrics";
+
+const apacheLogRecords = [];
+const apacheMetricRecords = [];
+const customAppRecords = [];
+const hostMetricRecords = [];
+const mongoLogRecords = [];
+const mongoMetricRecords = [];
+const nginxLogRecords = [];
+const nginxMetricRecords = [];
+const postgresLogRecords = [];
+const postgresMetricRecords = [];
+
+/**
+ * An array contain all record types.
+ * We iterate on this to write all records to the db easily.
+ */
+const allRecords = [
+  apacheLogRecords,
+  apacheMetricRecords,
+  customAppRecords,
+  hostMetricRecords,
+  mongoLogRecords,
+  mongoMetricRecords,
+  nginxLogRecords,
+  nginxMetricRecords,
+  postgresLogRecords,
+  postgresMetricRecords
+];
+
+/**
+ * Set up s3 client
+ */
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 /*
  * Configure timestream writeclient
@@ -25,16 +76,131 @@ const writeClient = new aws.TimestreamWrite({
 });
 
 /**
+ * The time injected into each record
+ * TODO: Replace this with a records unique timestamp
+ */
+const currentTime = Date.now().toString();
+
+function buildGenericRecord(record) {
+  return {
+    Dimensions: [
+      {
+        Name: 'host',
+        Value: 'andrew-laptop',
+      },
+      {
+        Name: 'IP',
+        Value: '192.1.1.1',
+      },
+      /*
+        * Generate a random number.
+        * Timestream will reject data with exact same dimensions.
+        */
+      {
+        Name: 'RANDOMNUMBER',
+        Value: `${Math.random() + Math.random() + Math.random()}`,
+      },
+    ],
+    MeasureName: 'rawData', // vector source
+    MeasureValue: JSON.stringify(record), // vector value
+    MeasureValueType: 'VARCHAR',
+    Time: currentTime.toString(),
+  };
+}
+
+const buildApacheLogRecord = (record) => {
+  apacheLogRecords.push(buildGenericRecord(record));
+};
+
+const buildApacheMetricRecord = (record) => {
+  apacheMetricRecords.push(buildGenericRecord(record));
+};
+
+const buildCustomAppRecord = (record) => {
+  customAppRecords.push(buildGenericRecord(record));
+};
+
+const buildHostMetricRecord = (record) => {
+  hostMetricRecords.push(buildGenericRecord(record));
+};
+
+const buildMongoLogRecord = (record) => {
+  mongoLogRecords.push(buildGenericRecord(record));
+};
+
+const buildMongoMetricRecord = (record) => {
+  mongoMetricRecords.push(buildGenericRecord(record));
+};
+
+const buildNginxLogRecord = (record) => {
+  nginxLogRecords.push(buildGenericRecord(record));
+};
+
+const buildNginxMetricRecord = (record) => {
+  nginxMetricRecords.push(buildGenericRecord(record));
+};
+
+const buildPostgresLogRecord = (record) => {
+  postgresLogRecords.push(buildGenericRecord(record));
+};
+
+const buildPostgresMetricRecord = (record) => {
+  postgresMetricRecords.push(buildGenericRecord(record));
+};
+
+/**
+ * Iterates through the records dividing them into their appropriate arrays
+ *
+ * @param {Object[]} rawData Records that were written to S3
+ */
+function buildRecordTypes(rawData) {
+  rawData.forEach(record => {
+    switch(record.type) {
+      case VECTOR_APACHE_LOGS_TYPE:
+        buildApacheLogRecord(record);
+        break;
+      case VECTOR_APACHE_METRICS_TYPE:
+        buildApacheMetricRecord(record);
+        break;
+      case VECTOR_CUSTOM_APPLICATION_TYPE:
+        buildCustomAppRecord(record);
+        break;
+      case VECTOR_HOST_METRICS_TYPE:
+        buildHostMetricRecord(record);
+        break;
+      case VECTOR_MONGO_LOGS_TYPE:
+        buildMongoLogRecord(record);
+        break;
+      case VECTOR_MONGO_METRICS_TYPE:
+        buildMongoMetricRecord(record);
+        break;
+      case VECTOR_NGINX_LOGS_TYPE:
+        buildNginxLogRecord(record);
+        break;
+      case VECTOR_NGINX_METRICS_TYPE:
+        buildNginxMetricRecord(record);
+        break;
+      case VECTOR_POSTGRES_LOGS_TYPE:
+        buildPostgresLogRecord(record);
+        break;
+      case VECTOR_POSTGRES_METRICS_TYPE:
+        buildPostgresMetricRecord(record);
+        break;
+    }
+  });
+}
+
+/**
  * Writes upto a 100 records to Timstream. Timestream can only accept upto
  * 100 records per write.
  *
- * @param {String[]} records The records that will be written
+ * @param {Object[]} records The records that will be written
  * @returns {Promise} The promise that records will be written to Timestream
  */
-async function write100Records(records) {
+async function write100Records(records,  tablename) {
   const params = {
-    DatabaseName: process.env.DATABASE_NAME,
-    TableName: process.env.DATABASE_TABLE,
+    DatabaseName: DATABASE_NAME,
+    TableName: tablename,
     Records: records,
   };
 
@@ -58,48 +224,25 @@ async function write100Records(records) {
 }
 
 /**
- * Writes all records to Timestream
+ * Iterates on `allRecords`. If a record type contains records,
+ * then write those records to Timestream.
  *
  * @param {Object[]} rawData - An array of records to write to Timestream
- * @returns {undefined}
+ * @returns {void}
  */
 async function writeRecords(rawData) {
   console.log('Writing records');
-  const currentTime = Date.now().toString(); // Unix time in milliseconds
-
-  let records = [];
-
-  rawData.forEach(evt => {
-    records = records.concat({
-      Dimensions: [
-        {
-          Name: 'host',
-          Value: 'andrew-laptop',
-        },
-        {
-          Name: 'IP',
-          Value: '192.1.1.1',
-        },
-        /*
-         * Generate a random number.
-         * Timestream will reject data with exact same dimensions.
-         */
-        {
-          Name: 'RANDOMNUMBER',
-          Value: `${Math.random() + Math.random() + Math.random()}`,
-        },
-      ],
-      MeasureName: 'rawData', // vector source
-      MeasureValue: JSON.stringify(evt), // vector value
-      MeasureValueType: 'VARCHAR',
-      Time: currentTime.toString(),
-    });
-  });
+  buildRecordTypes(rawData);
 
   const promises = [];
-  for (let i = 0; i <= records.length; i += 100) {
-    promises.push(write100Records(records.slice(i, i + 100)));
-  }
+  allRecords.forEach((recordType) => {
+    if (recordType.length === 0) return;
+
+    const tableName = records[0].type;
+    for (let i = 0; i <= records.length; i += 100) {
+      promises.push(write100Records(records.slice(i, i + 100), tableName));
+    }
+  });
 
   await Promise.all(promises);
 }
