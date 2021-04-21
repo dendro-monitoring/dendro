fdede76cee74248877fed8d8adedca57a1d8353d
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-04-21 17:43:05 -0400

    no longer waiting 10 seconds to setup firehose

--- src/aws/orchestrator/setupFirehose.ts
+++ src/aws/orchestrator/setupFirehose.ts
@@ -3,8 +3,6 @@ import store from '../../store';
 import { AWS_FIREHOSE_STREAM_NAME, AWS_S3_BUCKET_NAME } from '../../constants';
 
 export default async function setupFirehose(): Promise<any> {
-  // TODO don't do this
-  await new Promise(r => setTimeout(r, 10000));
   return AWSWrapper.createDeliveryStream(
     AWS_FIREHOSE_STREAM_NAME,
     AWS_S3_BUCKET_NAME,
