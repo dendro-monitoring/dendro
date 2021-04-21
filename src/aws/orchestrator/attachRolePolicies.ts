6d611fbf96c79f86c2f30d12dd0d4ab949d9a98c
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-04-21 09:25:59 -0400

    promisifying attachRolePolicies

--- src/aws/orchestrator/attachRolePolicies.ts
+++ src/aws/orchestrator/attachRolePolicies.ts
@@ -3,23 +3,23 @@ import constants from '../../constants';
 
 import { AWS_IAM_ROLE_NAME } from '../../constants';
 
-export default function attachRolePolicies(
-  resolve: () => any
-): void {
-  const promises: Promise<any>[] = [];
-  promises.push(attachRolePolicy(
-    AWS_IAM_ROLE_NAME, constants.FIREHOSE_POLICY_ARN
-  ));
-  promises.push(attachRolePolicy(
-    AWS_IAM_ROLE_NAME, constants.LAMBDA_POLICY_ARN)
-  );
-  promises.push(attachRolePolicy(
-    AWS_IAM_ROLE_NAME, constants.TIMESTREAM_POLICY_ARN)
-  );
-  promises.push(attachRolePolicy(
-    AWS_IAM_ROLE_NAME, constants.S3_POLICY_ARN)
-  );
-  Promise.all(promises).then(() => {
-    resolve();
+export default function attachRolePolicies(): Promise<void> {
+  return new Promise(resolve => {
+    const promises: Promise<any>[] = [];
+    promises.push(attachRolePolicy(
+      AWS_IAM_ROLE_NAME, constants.FIREHOSE_POLICY_ARN
+    ));
+    promises.push(attachRolePolicy(
+      AWS_IAM_ROLE_NAME, constants.LAMBDA_POLICY_ARN)
+    );
+    promises.push(attachRolePolicy(
+      AWS_IAM_ROLE_NAME, constants.TIMESTREAM_POLICY_ARN)
+    );
+    promises.push(attachRolePolicy(
+      AWS_IAM_ROLE_NAME, constants.S3_POLICY_ARN)
+    );
+    Promise.all(promises).then(() => {
+      resolve();
+    });
   });
 }
