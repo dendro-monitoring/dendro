7d4e07032d9a838c6b2dd5497f250f436e51efb0
Nicholas LiCalzi <32873455+nlicalzi@users.noreply.github.com>
2021-04-24 12:01:16 -0400

    CLI: `install-vector` command (#74)
    * added the CLI command  and implemented functionality for apt, rpm, and brew
    
    * added 'Other' option that refers the user to official Vector docs
    
    * Add color to install command
    
    Co-authored-by: Andrew <awcrotwell@gmail.com>

---
+++ src/commands/install-vector.ts
@@ -0,0 +1,38 @@
+import { Command } from '@oclif/command';
+import chalk from 'chalk';
+const { Select } = require('enquirer');
+import log from '../utils/log';
+
+export default class InstallVector extends Command {
+  static description = 'install Vector to collect and ship logs and metrics';
+
+  async run(): Promise<void> {
+    console.clear();
+    const instructions = 'Copy and paste the following command into another terminal instance to download and install Vector:';
+    const homebrew = ' brew tap timberio/brew && brew install vector && brew services restart vector ';
+    const rpm = ' sudo rpm -i https://packages.timber.io/vector/0.13.X/vector-0.13.X-1.x86_64.rpm && sudo systemctl restart vector ';
+    const apt = ' sudo apt-get install vector && sudo systemctl restart vector && vector top ';
+    const other = 'Please check the official Vector documentation at: https://vector.dev/docs/setup/installation/';
+    const codeBlock = (msg: string) => chalk.bgBlack.red(msg);
+
+    const prompt = new Select({
+      name: 'package-manager',
+      message: 'Which package manager do you use?',
+      choices: [ 'Homebrew', 'APT', 'RPM', 'Other' ]
+    });
+
+    const choice = await prompt.run();
+
+    log.info(instructions);
+    if (choice.includes('Homebrew')) {
+
+      console.log('   ' + codeBlock(homebrew));
+    } else if (choice.includes('APT')) {
+      console.log('   ' + codeBlock(apt));
+    } else if (choice.includes('RPM')) {
+      console.log('   ' + codeBlock(rpm));
+    } else if (choice.includes('Other')) {
+      console.log(other);
+    }
+  }
+}
