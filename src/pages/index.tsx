f866879a3100a80109c17c7accdb9b6a61b4dac2
Angel Ruiz-Bates <angelbates5@yahoo.com>
2021-04-27 20:16:26 -0400

    Show cloudwatch logs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
-import TrafficList from "../components/traffic";
-import ServicesList from "../components/Services/List";
+import TrafficList from '../components/traffic';
+import ServicesList from '../components/Services/List';
 import LogTable from '../components/LogTable/Table';
 
 import { MonitoredService } from '../constants/frontendTypes';
@@ -10,21 +10,34 @@ interface Props {
 }
 
 export default function Home({ services }: Props) {
-  const [logs, setLogs] = useState([]);
+  const [logs, setLogs] = useState({ logStreams: [{
+    logStreamName: '2021/04/27/[$LATEST]fabf572782014d7093c92acdf639435a',
+    creationTime: 1619559437780,
+    firstEventTimestamp: 1619559430810,
+    lastEventTimestamp: 1619559584705,
+    lastIngestionTime: 1619559593345,
+    uploadSequenceToken: '49609674868583835627285480401395376777896376309679495170',
+    arn: 'arn:aws:logs:us-east-1:141351053848:log-group:/aws/lambda/_deployableLambdaFunction:log-stream:2021/04/27/[$LATEST]fabf572782014d7093c92acdf639435a',
+    storedBytes: 0
+  },
+  {
+    logStreamName: '2021/04/27/[$LATEST]99f4cac93c4d40d795bf52a6fc91105a',
+    creationTime: 1619559421476,
+    firstEventTimestamp: 1619559416180,
+    lastEventTimestamp: 1619559496304,
+    lastIngestionTime: 1619559503849,
+    uploadSequenceToken: '49612743790005277477902513225953506941073727052862736322',
+    arn: 'arn:aws:logs:us-east-1:141351053848:log-group:/aws/lambda/_deployableLambdaFunction:log-stream:2021/04/27/[$LATEST]99f4cac93c4d40d795bf52a6fc91105a',
+    storedBytes: 0
+  }] });
   useEffect(() => {
     (async () => {
       // const res = await fetch('http://localhost:3000/api/logs');
       // const { logs: fetchedLogs } = await res.json();
 
-      const fetchedLogs: any = [
-        {
-          TIME: 1618341535723,
-          message: '2021-04-13T19:18:55.723Z\tundefined\tINFO\tLoading function\n',
-          ingestionTime: 1618341544834
-        }
-      ];
+      // console.log(fetchedLogs);
 
-      setLogs(fetchedLogs);
+      // setLogs(fetchedLogs);
     })();
   }, []);
 
