523cf43ce7db75dc0d6b241fdb87ea6a0ec37d13
nlicalzi <nicholas.licalzi@gmail.com>
2021-05-02 19:59:23 -0400

    added all charts for nginx, have to port to apache and finish queries

--- src/components/Services/NginxAccessLogs/Charts.tsx
+++ src/components/Services/NginxAccessLogs/Charts.tsx
@@ -1,26 +1,33 @@
-// import remap func
 import { useEffect, useState } from 'react';
 import { ChartDataPoint } from '../../../constants/frontendTypes';
 import ChartCard from '../ChartCard';
-import { VictoryChart, VictoryLine, VictoryLabel, VictoryAxis, VictoryTheme } from 'victory';
+import { VictoryChart, VictoryLine, VictoryBar, VictoryArea, VictoryStack, VictoryLabel, VictoryAxis } from 'victory';
 import formatTSQueryResult from '../formatTSQueryResult';
 const Series = require('time-series-data-generator');
 
-function loadRpsData(setterFunc: any) {
+function generateGaussianData(interval: number, mean: number, variance: number) {
   const d = new Date();
   const until = new Date(Date.now()).toISOString(); // today
   const from = new Date(d.setDate(d.getDate() - 7)).toISOString(); // a week ago
-  const interval = 1 * 60 * 60; // hours, minutes, seconds
   const keyName = 'y';
   const series = new Series({ from, until, interval, keyName });
 
-  const data = series.gaussian({ mean: 250, variance: 85 }).map((record: any) => {
+  const data = series.gaussian({ mean, variance }).map((record: any) => {
     const newRecord = {};
     newRecord.x = new Date(record.timestamp).valueOf(); // convert ISO string to a date and get UNIX time
     newRecord.y = record.y;
     return newRecord;
   });
 
+  return data;
+}
+
+function loadRpsData(setterFunc: any) {
+  const interval = 60 * 60;
+  const mean= 250;
+  const variance = 75;
+
+  const data = generateGaussianData(interval, mean, variance);
   setterFunc(data);
 
   // (async () => {
@@ -45,18 +52,12 @@ function loadRpsData(setterFunc: any) {
 }
 
 function loadLatencyData(setterFunc: any) {
-  const d = new Date();
-  const until = new Date(Date.now()).toISOString(); // today
-  const from = new Date(d.setDate(d.getDate() - 7)).toISOString(); // a week ago
-  const interval = 7 * 24 * 60 * 60 ; // hours * minutes * seconds
-  const keyName = 'y';
-  const series = new Series({ from, until, interval, keyName });
-  const data = series.gaussian({ mean: 300, variance: 75 }).map((record: any) => {
-    const newRecord = {};
-    newRecord.x = new Date(record.timestamp).valueOf(); // convert ISO string to a date and get UNIX time
-    newRecord.y = record.y;
-    return newRecord;
-  });
+  const interval = 2 * 60 * 60;
+  const mean = .200;
+  const variance = .03;
+
+  const data = generateGaussianData(interval, mean, variance);
+  setterFunc(data);
 
   // (async () => {
   //   const res = await fetch(
@@ -83,13 +84,38 @@ function loadLatencyData(setterFunc: any) {
   // })();
 }
 
+function loadStatusCodeData(setterFunc: any) {
+  const data200 = generateGaussianData(60 * 60, .200, .03);
+  const data400 = generateGaussianData(60 * 60, .110, .01);
+  const data500 = generateGaussianData(60 * 60, .120, .021);
+
+  setterFunc([data200, data400, data500]);
+  // (async () => {
+  //   const res = await fetch(
+  //     '/api/query',
+  //     {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         query: ''
+  //       })
+  //     }
+  //   );
+
+  //   const { data: fetchedData } = await res.json();
+  //   const formattedResult = formatTSQueryResult(fetchedData);
+  //   setterFunc(formattedResult);
+  // })();
+}
+
 export default function Chart({ name }: { name: string }) {
-  const [rpsData, setRpsData] = useState([]);         // single array
-  const [latencyData, setLatencyData] = useState([]); // this will have 4 sub-array elements
+  const [rpsData, setRpsData] = useState([]);               // single array
+  const [latencyData, setLatencyData] = useState([]);       // this will have 4 sub-array elements
+  const [statusCodeData, setStatusCodeData] = useState([]); // 3 sub arrays: 200 level, 400 level, 500 level
 
   useEffect(() => {
     loadRpsData(setRpsData);
     loadLatencyData(setLatencyData);
+    loadStatusCodeData(setStatusCodeData);
   }, []);
 
   return (
@@ -101,14 +127,13 @@ export default function Chart({ name }: { name: string }) {
           {/* Chart 1 */}
           <VictoryChart
             style={{ parent: { maxWidth: '50%', } }}
-            // theme={VictoryTheme.material}
             scale={{ x: 'time', y: 'linear' }}
             domain={{ y: [0, 600] }}
             key='rps'
           >
             <VictoryLabel text="Requests per Second (past 7 days)" x={230} y={30} textAnchor="middle"/>
             <VictoryLine
-              style={{ data: { stroke: '#8dd9d4' }, parent: { border: '1px solid #ccc' } }}
+              style={{ data: { stroke: '#98DED9' }, parent: { border: '1px solid #ccc' } }}
               data={rpsData}
             />
             <VictoryAxis
@@ -123,16 +148,24 @@ export default function Chart({ name }: { name: string }) {
           {/* Chart 2 */}
           <VictoryChart
             style={{ parent: { maxWidth: '50%', } }}
-            // theme={VictoryTheme.material}
+            scale={{ x: 'time', y: 'linear' }}
+            domain={{ y: [0, 1] }}
             key='latency'
           >
-            <VictoryLabel text={'Avg. HTTP Response Time (ms)'} x={230} y={30} textAnchor="middle"/>
+            <VictoryLabel text={'Avg. HTTP Response Time (s)'} x={230} y={30} textAnchor="middle"/>
             <VictoryLine
               style={{
-                data: { stroke: '#EF4444' },
+                data: { stroke: '#98DED9' },
                 parent: { border: '1px solid #9CA3AF' }
               }}
               data={latencyData}
+            />
+            <VictoryAxis
+              tickFormat={t => `${t.getUTCMonth() + 1}/${t.getUTCDate()}`}
+              style={{ tickLabels: { fontSize: 12, padding: 5 } }}
+            />
+            <VictoryAxis dependentAxis
+              style={{ tickLabels: { fontSize: 12, padding: 5 } }}
             />
           </VictoryChart>
         </>
@@ -142,32 +175,41 @@ export default function Chart({ name }: { name: string }) {
           {/* Chart 3 */}
           <VictoryChart
             style={{ parent: { maxWidth: '50%', } }}
-            // theme={VictoryTheme.material}
+            domainPadding={{ x: 10 }}
             key='slowest'
           >
-            <VictoryLabel text={'Slowest Paths (ms)'} x={230} y={30} textAnchor="middle"/>
-            <VictoryLine
-              style={{
-                data: { stroke: '#EF4444' },
-                parent: { border: '1px solid #9CA3AF' }
-              }}
-            // data={chartData1}
+            <VictoryLabel text={'Slowest Paths (s)'} x={230} y={30} textAnchor="middle"/>
+            <VictoryBar horizontal
+              style={{ data: { fill: '#161D6F' } }}
+              data={ [
+                { x: '/boo', y: .2 },
+                { x: '/bleh', y: .5 },
+                { x: '/blah', y: .8 },
+                { x: '/api', y: 1.3 },
+                { x: '/index', y: 1.8 },
+              ] }
             />
           </VictoryChart>
 
           {/* Chart 4 */}
           <VictoryChart
             style={{ parent: { maxWidth: '50%', } }}
-            // theme={VictoryTheme.material}
+            scale={{ x: 'time', y: 'linear' }}
+            domain={{ y: [0, 1] }}
             key='status'
           >
-            <VictoryLabel text={'Status Code Responses'} x={240} y={30} textAnchor="middle"/>
-            <VictoryLine
-              style={{
-                data: { stroke: '#EF4444' },
-                parent: { border: '1px solid #9CA3AF' }
-              }}
-            // data={chartData1}
+            <VictoryLabel text={'Status Code Responses'} x={230} y={30} textAnchor="middle"/>
+            <VictoryStack colorScale={['#161D6F', '#c7ffd8', '#98DED9']} >
+              <VictoryArea data={statusCodeData[0]} />
+              <VictoryArea data={statusCodeData[1]} />
+              <VictoryArea data={statusCodeData[2]} />
+            </VictoryStack>
+            <VictoryAxis
+              tickFormat={t => `${t.getUTCMonth() + 1}/${t.getUTCDate()}`}
+              style={{ tickLabels: { fontSize: 12, padding: 5 } }}
+            />
+            <VictoryAxis dependentAxis
+              style={{ tickLabels: { fontSize: 12, padding: 5 } }}
             />
           </VictoryChart>
         </>
@@ -175,37 +217,3 @@ export default function Chart({ name }: { name: string }) {
     </>
   );
 }
-
-// SECOND QUERY
-// (async () => {
-//   const res = await fetch(
-//     '/api/query',
-//     { method: 'POST', body: JSON.stringify({ query: 'SELECT * FROM DendroTimestreamDB.postgres-metrics LIMIT 10' }) }
-//   );
-
-//   const { data: fetchedData } = await res.json();
-//   setData(fetchedData);
-// })();
-
-// THIRD QUERY
-// (async () => {
-//   const res = await fetch(
-//     '/api/query',
-//     { method: 'POST', body: JSON.stringify({ query: 'SELECT * FROM DendroTimestreamDB.postgres-metrics LIMIT 10' }) }
-//   );
-
-//   const { data: fetchedData } = await res.json();
-//   setData(fetchedData);
-// })();
-
-// FOURTH QUERY
-// (async () => {
-//   const res = await fetch(
-//     '/api/query',
-//     { method: 'POST', body: JSON.stringify({ query: 'SELECT * FROM DendroTimestreamDB.postgres-metrics LIMIT 10' }) }
-//   );
-
-//   const { data: fetchedData } = await res.json();
-//   setData(fetchedData);
-// })();
-
