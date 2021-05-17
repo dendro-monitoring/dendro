12b2c4185cdcd776be851f81f74a757bbd212cce
Peter D'Elia <petertdelia@gmail.com>
2021-05-17 14:50:11 -0400

    implemented retry in delete bucket

--- src/aws/s3/deleteBucket.ts
+++ src/aws/s3/deleteBucket.ts
@@ -1,18 +1,28 @@
 import { AWSError } from 'aws-sdk';
 import { AWS_S3 } from '../../constants';
+import retry from '../../utils/retry';
 
 export default function deleteBucket(Bucket: string): Promise<any> {
-  return new Promise(resolve => {
+  return new Promise(async resolve => {
     const bucketParams = {
       Bucket,
     };
 
-    AWS_S3.deleteBucket(bucketParams, (err: AWSError) => {
-      if (err && err.code === 'NoSuchBucket') {
-        resolve(err);
-      } else if (err) {
-        throw new Error(String(err));
-      } else resolve(null);
-    });
+    let finished = false;
+    await retry(13, () => {
+      AWS_S3.deleteBucket(bucketParams, (err: AWSError) => {
+        if (err && err.code !== 'BucketNotEmpty') {
+          resolve(err);
+        } else if (!err || err.code === "NoSuchBucket") {
+          finished = true;
+          resolve(null);
+        }
+      })
+    }, finished);
+
+    if (!finished) {
+      throw new Error('Could not delete Bucket!');
+    }
+
   });
 }
