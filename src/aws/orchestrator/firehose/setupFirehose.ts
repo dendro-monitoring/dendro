cc80d302300eea61016462a93e38877b5ce0eb3a
Peter D'Elia <petertdelia@gmail.com>
2021-05-19 10:20:18 -0400

    expanded retries for firehose deploy (#120)

--- src/aws/orchestrator/firehose/setupFirehose.ts
+++ src/aws/orchestrator/firehose/setupFirehose.ts
@@ -4,8 +4,6 @@ import { AWS_FIREHOSE_STREAM_NAME, AWS_S3_BUCKET_NAME } from '../../../constants
 import getBucketName from '../s3/getBucketName';
 
 export default async function setupFirehose(): Promise<any> {
-  // TODO don't do this
-  // await new Promise(r => setTimeout(r, 12500));
   return AWSWrapper.createDeliveryStream(
     AWS_FIREHOSE_STREAM_NAME,
     await getBucketName() || AWS_S3_BUCKET_NAME,
