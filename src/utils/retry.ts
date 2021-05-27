731f592630f54e435923b6ed546982a744afd0ff
Peter D'Elia <petertdelia@gmail.com>
2021-05-27 13:04:44 -0400

    long polling now has exponential backoff

--- src/utils/retry.ts
+++ src/utils/retry.ts
@@ -1,11 +1,15 @@
-export default function asyncRetry(numberOfRetries: number, callback: (input: string) => void, finished: boolean, timeToWait: number = 1000): Promise<void> {
+export default function asyncRetry(numberOfRetries: number, callback: (input: string) => boolean, finished: boolean, timeToWait: number = 1000): Promise<void> {
   return new Promise(async resolve => {
     let retriesArray = Array(numberOfRetries).fill(true);
     for (let item in retriesArray) {
+      finished = await callback(item);
+      console.log("finished: ", finished)
+      console.log("time to wait: ", timeToWait);
       if (finished) return resolve();
-      await callback(item);
+      console.log("waiting after return resolve before setTimeout...")
       await new Promise((resolve) => setTimeout(resolve, timeToWait));
+      timeToWait *= 2;
     }
-    resolve();
+    return resolve();
   });
 }
