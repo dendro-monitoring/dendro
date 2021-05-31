2545fda072d95beabba3c36a88d30d05a92abb08
angeljruiz <angelbates5@yahoo.com>
2021-05-30 21:26:09 -0400

    Fix charts page crash (#126)

--- src/pages/api/tables.ts
+++ src/pages/api/tables.ts
@@ -1,9 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import listTables from '../../aws/orchestrator/timestream/listTables';
+import listDatabases from '../../aws/timestream/listDatabases';
 import { MonitoredService, VectorService } from '../../constants/frontendTypes';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
   try {
+    if ((await listDatabases()).length === 0) return res.status(200).json({ data: '' });
+
     const tables = await listTables();
 
     const data: MonitoredService[] = [];
